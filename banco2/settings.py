9d8e7f963f128259e0caab85553b23f23bdb21f0
Tomás Saravia <102377326+tomas-sa@users.noreply.github.com>
2023-07-28 21:29:24 -0300

    Update settings.py

--- banco2/settings.py
+++ banco2/settings.py
@@ -184,5 +184,5 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['https://bankfrontsara.netlify.app']
+CORS_ALLOWED_ORIGINS = ['https://bankfrontsara.netlify.app', 'http://192.168.0.8:5173']
 AUTH_USER_MODEL = 'user.CustomUser'
